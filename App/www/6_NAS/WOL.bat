2045d58ec48e2a67c5d1a2513cb270c67f400333
Antonio Castañeda Gomez <antonio.castaneda-gomez@uni-wuerzburg.de>
2023-02-27 12:48:31 +0100

    NasUpdate

---
+++ App/www/6_NAS/WOL.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set url="http://132.187.202.171:5000/"
+
+echo Waking up NAS!
+
+start "" wakeup.exe 90:09:D0:19:67:03
+
+echo Firefox will start after timeout!
+
+timeout /t 30
+
+start firefox.exe %url%
+
+exit
+
+:: wake up https://sites.google.com/site/sysutil/wakeup
