f8919d2befee3923314ba0c60304d6e483088cec
ajcastanedag <antonio.castanedag@gmail.com>
2024-02-21 17:20:28 +0100

    Wizard TAb

---
+++ App/temporaltabs/Wizard/Wizard.R
@@ -0,0 +1,313 @@
+#########################    UAS JMU PLATFORM    ############################### ----
+# Do not close when setwd() fails.
+# Add reset all fields button.
+# Fill markdown files with information (from drive)
+# 
+################################################################################
+##### Load libraries                                                            -----
+pacman::p_load("shiny","shinyWidgets", "stringr","shinyjs", "shinythemes", "shinyFiles",
+               "leaflet","leaflet.extras", "tidyverse", "rmarkdown", "shinyBS",
+               "easycsv","sf","sfheaders","shinyalert","threejs")
+################################################################################
+Root <- paste0(getwd(),"/App/")
+##### Add resource path                                                         ----- 
+addResourcePath(prefix = 'media', directoryPath = paste0(Root,"/www"))
+##### Include Functions file-> IF NOT SPECIFIED LIDAR COMPUTER FILE WILL BE USED----- 
+source(paste0(Root,"/www/3_Functions/Base.R"))
+##### Possible output locations general directory (E drive)                     ----- 
+#TargetDrive() <- paste0("/home/antonio/Desktop/")
+##### Set path to general style                                                 ----- 
+Style <- paste0(Root,"/www/2_Style/UAS_Style_AJCG.css")
+################################################################################
+# https://github.com/statnmap/RshinyApps/tree/master/On-Off_SwitchButton
+################################################################################
+
+
+
+ui <- tagList(
+  tags$head(
+    # Include our custom CSS
+    includeCSS(Style)
+    #tags$link(rel = "stylesheet", type = "text/css", href = "/www/2_Style/UASstyle.css")
+  ),
+  useShinyjs(),
+  navbarPage(title = div(img(src='media/4_Graphs/Logo.png',
+                             style="margin-top: -10px; padding-right:10px; padding-bottom:10px",
+                             height = 50)),
+             windowTitle="JMU UAS Flight book",
+             theme = shinytheme("slate"),
+             ###################################################################
+             # Create                                                           ----  
+             tabPanel("Processing Wizzard",
+                      # Include our custom CSS
+                      tags$head(includeCSS(Style)),
+                      icon = icon("hat-wizard"),
+                      sidebarLayout(
+                        sidebarPanel(width = 4,
+                                     tags$div(title="HelpText.............",
+                                              h4(strong("Select parameters"),
+                                                 align = "center"),
+                                     ),
+                                     
+                                     tags$div(title="HelpText.............",
+                                              h5(strong("Basic Data"),
+                                                 align = "left"),
+                                     ),
+                                     
+                                     splitLayout(
+                                       cellWidths = c("50%", "50%"),
+                                       div(
+                                         actionButton("WsI", "WSI", 
+                                                      #style = 'margin-top:23px',
+                                                      size ="lg",
+                                                      width = "100%")
+                                       ),
+                                       div(
+                                         actionButton("WsII", "WSII", 
+                                                      #style = 'margin-top:23px',
+                                                      size ="lg",
+                                                      width = "100%")
+                                       )
+                                     ),
+                                     
+                                     br(),
+                                     
+                                     textInput("calibrate_dir", "Calibration pannel",
+                                               value = ""),
+
+                                     splitLayout(
+                                       cellWidths = c("50%", "50%"),
+                                       div(
+                                         style = "margin-top: 2px;",
+                                         selectizeInput(
+                                           "name",
+                                           label = "Mission",
+                                           choices = list(""),
+                                           selected = ".",
+                                           options = list(dropdownParent = 'body')
+                                         )
+                                       ),
+                                       div(
+                                         style = "margin-top: 2px;",
+                                         selectizeInput(
+                                           "name",
+                                           label = "Flight",
+                                           choices = list(""),
+                                           selected = ".",
+                                           options = list(dropdownParent = 'body')
+                                         )
+                                       )
+                                     ),
+                                     
+                                     tags$div(title="HelpText.............",
+                                              h5(strong("Reconstruction quality"),
+                                                 align = "left"),
+                                     ),
+                                     
+                                     splitLayout(
+                                       cellWidths = c("35%", "35%", "30%"),
+                                       div(
+                                         style = "margin-top: 2px;",
+                                         selectizeInput(
+                                           "acc",
+                                           label = "Quality",
+                                           choices = c("Ultra High","High","Medium"),
+                                           selected = "High",
+                                           options = list(dropdownParent = 'body')
+                                         )
+                                       ),
+                                       div(
+                                         style = "margin-top: 2px;",
+                                         selectizeInput(
+                                           "filter_mode",
+                                           label = "Filtering",
+                                           choices = list("No","Mild","Moderate","Agressive"),
+                                           selected = "Mild",
+                                           options = list(dropdownParent = 'body')
+                                         )
+                                       ),
+                                       div(
+                                         textInput("crs2", label="Coordinate System", value = "4326", width = NULL, placeholder = NULL)
+                                       )
+                                     ),
+                                     
+                                     splitLayout(
+                                       cellWidths = c("50%", "50%"),
+                                       div(
+                                         switchButton(inputId = "reflectance_pan",
+                                                      label = "Use calibration panel",
+                                                      col = "GB",
+                                                      value = TRUE)
+                                         ),
+                                       div(
+                                         switchButton(inputId = "reflectance_dls",
+                                                      label = "Use DLS",
+                                                      col = "GB",
+                                                      value = FALSE)
+                                       )
+                                     ),
+                                     
+                                     tags$div(title="HelpText.............",
+                                              h5(strong("Aligh Photos"),
+                                                 align = "left"),
+                                     ),
+                                     
+                                     splitLayout(
+                                       cellWidths = c("50%", "50%"),
+                                       div(
+                                         sliderInput("keypoint_limit", "Keypoint Limit (10e3):",
+                                                     min = 10, max = 80,
+                                                     value = 40 , step = 1,
+                                                     ticks = F, width = "90%")
+                                       ),
+                                       div(sliderInput("tiepoint_limit", "Tiepoint Limit (10e3):",
+                                                       min = 1, max = 20,
+                                                       value = 4 , step = 1,
+                                                       ticks = F, width = "90%")
+                                       )
+                                     ),
+                                     
+                                     splitLayout(
+                                       cellWidths = c("50%", "50%"),
+                                       div(
+                                         switchButton(inputId = "guided_matching",
+                                                      label = "Guided matching:",
+                                                      col = "GB",
+                                                      value = FALSE)
+                                       ),
+                                       div(
+                                         switchButton(inputId = "adaptive_fitting",
+                                                      label = "Adaptive fitting:",
+                                                      col = "GB",
+                                                      value = TRUE)
+                                       )
+                                     ),
+                                     
+                                     
+                                     tags$div(title="HelpText.............",
+                                              h5(strong("Orthomosaic Options"),
+                                                 align = "left"),
+                                     ),
+                                     
+                                     splitLayout(
+                                       cellWidths = c("50%", "50%"),
+                                       div(
+                                         switchButton(inputId = "ghosting_filter",
+                                                      label = "Ghosting filter:",
+                                                      col = "GB",
+                                                      value = FALSE)
+                                       ),
+                                       div(
+                                         switchButton(inputId = "refine_seamlines",
+                                                      label = "Refine seamlines:",
+                                                      col = "GB",
+                                                      value = TRUE)
+                                       )
+                                     ),
+                                     
+                                     
+                                     
+                                     
+                                     br(),
+                                     actionButton("addBtn", NULL, icon = icon("plus"),
+                                                  #style = 'margin-top:23px',
+                                                  size ="lg",
+                                                  width = "100%")
+                        ),
+                        
+                        mainPanel(tags$div(title="Main Data table: All values stored here will be used to generate folder structure. Please check and modify if needed by double click on the fields.",
+                                           DT::dataTableOutput("Items")
+                        ),
+                        
+                        tags$div(title="Click here when you have added all flights to the table.",
+                                 actionButton("exportitems",
+                                              "Please add items",
+                                              style = 'margin-top:23px',
+                                              width = "100%"),
+                        ))
+                      )),
+             ###################################################################
+             
+  )
+)
+
+
+# 
+# 
+# 
+# 
+# # Define UI
+# ui <- fluidPage(
+#   titlePanel("Equipment Usage Registration"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       
+#       splitLayout(
+#         cellWidths = c("40%", "40%"),
+#         div(
+#           textInput("name", "Name:"),
+#           textInput("email", "Email:")
+#           )
+#         ),
+#       
+#       
+#       dateInput("date", "Date:", value = Sys.Date()),
+#       textInput("id", "Object ID:"),
+#       actionButton("addBtn", "Add")
+#     ),
+#     mainPanel(
+#       dataTableOutput("table"),
+#       actionButton("exportBtn", "Export to Excel")
+#     )
+#   )
+# )
+
+# Define server logic
+server <- function(input, output) {
+  # Initialize empty data frame to store registration info
+  data <- reactiveVal(data.frame(Code = character(),
+                                 Name = character(),
+                                 Date = as.Date(character()),
+                                 Object = character()))
+  
+  # Add registration info to the data frame
+  observeEvent(input$addBtn, {
+    newRow <- isolate(c(input$id, input$name, input$date, input$email))
+    if (nchar(trimws(newRow[1])) > 0) {
+      data(rbind(data(), as.data.frame(t(newRow))))
+    } else {
+      showModal(modalDialog(
+        title = "Error",
+        "Please fill in all the fields.",
+        easyClose = TRUE
+      ))
+    }
+  })
+  
+  # Render the table
+  output$table <- renderDataTable({
+    data()
+  })
+  
+  # Export data to Excel
+  observeEvent(input$exportBtn, {
+    if (nrow(data()) > 0) {
+      exportFile <- tempfile(fileext = ".xlsx")
+      write.xlsx(data(), exportFile)
+      showModal(modalDialog(
+        title = "Export Complete",
+        paste("The data has been exported to", exportFile),
+        easyClose = TRUE
+      ))
+    } else {
+      showModal(modalDialog(
+        title = "Error",
+        "No data available to export.",
+        easyClose = TRUE
+      ))
+    }
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
--- App/www/3_Functions/Base.R
+++ App/www/3_Functions/Base.R
@@ -76,18 +76,9 @@ CreateFolder <- function(dir_path, DataFrame) {
       # Create sub directories 0_Images, 1_Agisoft, 2_Reports, 3_FlightFiles, 4_RawOutput
       subdirectories <- c("0_Images", "1_Agisoft", "2_Reports", "3_FlightFiles", "4_RawOutput")
       
-      # Create Conditional for thermal images vs RGB 
-      if ("0_Images" %in% subdirectories && grepl("3TAgisoft",selected_system)) {
-        flight_files_subdirs <- c("0_RGB", "1_Thermal", "3_ThermalCal")
-      }
-      
-      
-      
-      
       # Create subdirectories inside 3_FlightFiles
       if ("3_FlightFiles" %in% subdirectories) {
         flight_files_subdirs <- c("0_Log", "1_Plan", "2_Other")
-      }
       
       for (sub_dir_name in subdirectories) {
         sub_dir <- file.path(project_dir, sub_dir_name)
@@ -108,8 +99,23 @@ CreateFolder <- function(dir_path, DataFrame) {
             dir.create(flight_sub_dir)
           }
         }
+
+        # Create subdirectories inside 3_FlightFiles
+        if (sub_dir_name == "0_Images" && grepl("3TAgisoft",selected_system)) {
+          
+          Thermal_subdirs <- c("0_RGB", "1_Thermal", "3_ThermalCal")
+          
+          for (flight_subdir_name in Thermal_subdirs) {
+            flight_sub_dir <- file.path(sub_dir, flight_subdir_name)
+            dir.create(flight_sub_dir)
+          }
+          
+        }
+        
       }
-      
+        
+      }
+        
       return(TRUE)  # Success
     } else {
       return(FALSE)  # Folder structure already exists
@@ -258,7 +264,7 @@ CreateFolder <- function(dir_path, DataFrame) {
       
       # Create subdirectories inside 3_FlightFiles
       if ("1_Images" %in% subdirectories) {
-        Images_subdirs <- c("Thermal", "Thermal_Cal", "RGB")
+        Images_subdirs <- c("0_RGB", "1_Thermal", "3_ThermalCal")
       }
       
       for (sub_dir_name in subdirectories) {
@@ -445,4 +451,82 @@ GeneratePol <- function(GeomSF){
   return(SfObj)
 }
 ################################################################################
+# Customised TRUE-FALSE switch button for Rshiny
+# Only sing CSS3 code (No javascript)
 #
+# Sébastien Rochette
+# http://statnmap.com/en/
+# April 2016
+#
+# CSS3 code was found on https://proto.io/freebies/onoff/
+# For CSS3 customisation, refer to this website.
+
+#' A function to change the Original checkbox of rshiny
+#' into a nice true/false or on/off switch button
+#' No javascript involved. Only CSS code.
+#' 
+#' To be used with CSS script 'button.css' stored in a 'www' folder in your Shiny app folder
+#' 
+#' @param inputId The input slot that will be used to access the value.
+#' @param label Display label for the control, or NULL for no label.
+#' @param value Initial value (TRUE or FALSE).
+#' @param col Color set of the switch button. Choose between "GB" (Grey-Blue) and "RG" (Red-Green)
+#' @param type Text type of the button. Choose between "TF" (TRUE - FALSE), "OO" (ON - OFF) or leave empty for no text.
+
+switchButton <- function(inputId, label, value=FALSE, col = "GB", type="TF") {
+  
+  # color class
+  if (col != "RG" & col != "GB") {
+    stop("Please choose a color between \"RG\" (Red-Green) 
+      and \"GB\" (Grey-Blue).")
+  }
+  if (!type %in% c("OO", "TF", "YN")){
+    warning("No known text type (\"OO\", \"TF\" or \"YN\") have been specified, 
+     button will be empty of text") 
+  }
+  if(col == "RG"){colclass <- "RedGreen"}
+  if(col == "GB"){colclass <- "GreyBlue"}
+  if(type == "OO"){colclass <- paste(colclass,"OnOff")}
+  if(type == "TF"){colclass <- paste(colclass,"TrueFalse")}
+  if(type == "YN"){colclass <- paste(colclass,"YesNo")}
+  
+  # No javascript button - total CSS3
+  # As there is no javascript, the "checked" value implies to
+  # duplicate code for giving the possibility to choose default value
+  
+  if(value){
+    tagList(
+      tags$div(class = "form-group shiny-input-container",
+               tags$div(class = colclass,
+                        tags$label(label, class = "control-label"),
+                        tags$div(class = "onoffswitch",
+                                 tags$input(type = "checkbox", name = "onoffswitch", class = "onoffswitch-checkbox",
+                                            id = inputId, checked = ""
+                                 ),
+                                 tags$label(class = "onoffswitch-label", `for` = inputId,
+                                            tags$span(class = "onoffswitch-inner"),
+                                            tags$span(class = "onoffswitch-switch")
+                                 )
+                        )
+               )
+      )
+    )
+  } else {
+    tagList(
+      tags$div(class = "form-group shiny-input-container",
+               tags$div(class = colclass,
+                        tags$label(label, class = "control-label"),
+                        tags$div(class = "onoffswitch",
+                                 tags$input(type = "checkbox", name = "onoffswitch", class = "onoffswitch-checkbox",
+                                            id = inputId
+                                 ),
+                                 tags$label(class = "onoffswitch-label", `for` = inputId,
+                                            tags$span(class = "onoffswitch-inner"),
+                                            tags$span(class = "onoffswitch-switch")
+                                 )
+                        )
+               )
+      )
+    ) 
+  }
+}
